96f4cb313fcbffd476549851e5e8d029bccc54ae
cristianlopezcp63@gmail.com <lopezgaray43@gmail.com>
2025-05-13 14:25:59 -0500

    Actualizacion en la barra del menu, logos puestos, funcionalidad de vistas echa

---
+++ app/src/main/java/com/example/volleygo/ui/VisitorTeam/VisitorTeamFragment.java
@@ -0,0 +1,40 @@
+package com.example.volleygo.ui.VisitorTeam;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.volleygo.databinding.FragmentHomeBinding;
+
+
+
+public class VisitorTeamFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        VisitorTeamViewModel VisitorTeamViewModel =
+                new ViewModelProvider(this).get(VisitorTeamViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        VisitorTeamViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
